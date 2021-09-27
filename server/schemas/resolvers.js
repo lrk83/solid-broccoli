7badf0526e83bdb298ccb05d69f67dd0f06fb1db
Lukas Keel <lrk83@cornell.edu>
2021-09-27 13:25:38 -0400

    User schemas

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,5 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
+const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
@@ -14,6 +14,35 @@ const resolvers = {
             }
 
             throw new AuthenticationError('Not logged in')
+        }
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+      
+            return { token, user };
+        },
+        saveBook: async(parent, args, context) => {
+            if (context.user) {
+                const thought = await Book.create()
+            }
         }
     }
 };
